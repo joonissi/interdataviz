ac5ad851c113b4f5770d4e37c63558906ebf41d1
joonissi <joonanissinen123@gmail.com>
2018-04-20 15:27:01 +0300

    articles

--- data/data.py
+++ data/data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 articles_feb = pd.read_csv('NYTArticlesFeb2017.csv')
 
@@ -9,12 +10,23 @@ kw = articles_feb.keywords
 timestamp = articles_feb.pubDate
 
 f = open('article_keywords_and_dates.csv','w')
-f.write('keywords,timestamp' + '\r\n')
+f.write('keywords,articleID,timestamp' + '\r\n')
 
 for index, row in articles_feb.iterrows():
   
-  print '\"' + row['keywords'][1:-1].replace('\'','').replace('"','\'') + '\"' + ',' + row['pubDate']
-  line = '\"' + row['keywords'][1:-1].replace('\'','').replace('"','\'') + '\"' + ',' + row['pubDate']
-  f.write(line + '\r\n')
+  #print '\"' + row['keywords'][1:-1].replace('\'','').replace('"','\'') + '\"' + ',' + row['pubDate']
+  #line = '\"' + row['keywords'][1:-1].replace('\'','').replace('"','\'') + '\"' + ',' + row['pubDate']
+  #f.write(line + '\r\n')
+
+  s = row['keywords'][1:-1].replace('\'','').replace('"','\'')
+  ss = [x.strip() for x in s.split(',')]
+  #print ss
+
+  for i in ss:
+    sss =  i + ',' + row['articleID'] + ',' + row['pubDate']
+    print sss
+    f.write(sss + '\r\n')
 
 f.close()
+
+#print articles_feb.keys()
